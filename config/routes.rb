5671bc5636ab9e6b8ff5d0d3e81f38dfbfe0e5e7
Patrick Ranchez <ranchezpatrick@gmail.com>
2022-03-08 22:10:33 +0800

    added set-up for devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
