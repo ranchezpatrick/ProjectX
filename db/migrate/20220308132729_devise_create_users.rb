f432d14dd06fc55e268b92e5166ef11b00e75970
Patrick Ranchez <ranchezpatrick@gmail.com>
2022-03-08 22:37:26 +0800

    fixed seeds and migrations

--- db/migrate/20220308132729_devise_create_users.rb
+++ db/migrate/20220308132729_devise_create_users.rb
@@ -20,7 +20,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
-
+      t.string :role
+      t.string :first_name
+      t.string :last_name
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
@@ -31,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      
 
       t.timestamps null: false
     end
